fdbfa725a5590dab1687101446b9f83a7f3ec73d
Kagan <kaganmert@hotmail.com.tr>
2022-04-08 14:37:57 +0300

    Add workflow

---
+++ README.md
@@ -0,0 +1,25 @@
+# Data Visualization App
+
+Single page web app which visualizes live MQTT broker metrics.
+
+Demo live at: [digiterra.kagan.dev](https://digiterra.kagan.dev/)
+
+## Features
+
+- Responsive
+- UI Components
+- TailwindCSS
+- Standardized Data Hooks
+- MQTT Connection
+
+## Configuration
+
+### How to change environment file
+
+The setup for Data Visualization App would look like this for example:
+
+```
+REACT_APP_CLIENT_ID="******"
+REACT_APP_RECONNECT_PERIOD=xxxx
+REACT_APP_CONNECTION="ws://xxxx:PORT/mqtt"
+```
--- src/utils/StoragePersister.js
+++ src/utils/StoragePersister.js
@@ -1,12 +1,18 @@
 import moment from "moment";
 
 function StoragePersister(data) {
-  let clientsArr = [];
   let datesArr = [];
+  let clientsArr = [];
+  let connectedArr = [];
+  let disconnectedArr = [];
+  console.log(data.disconnected);
 
-  if (!localStorage.getItem("activeSubscriptions")) {
+  if (!localStorage.getItem("dates")) {
     datesArr.push(moment().format("hh:mm:ss"));
     localStorage.setItem("dates", JSON.stringify(datesArr));
+
+    connectedArr.push(data.connected);
+    localStorage.setItem("connected", JSON.stringify(connectedArr));
 
     clientsArr.push(data.activeSubscriptions);
     localStorage.setItem("activeSubscriptions", JSON.stringify(clientsArr));
@@ -14,6 +20,10 @@ function StoragePersister(data) {
     datesArr = JSON.parse(localStorage.getItem("dates"));
     datesArr.push(moment().format("hh:mm:ss"));
     localStorage.setItem("dates", JSON.stringify(datesArr));
+
+    connectedArr = JSON.parse(localStorage.getItem("connected"));
+    connectedArr.push(data.connected);
+    localStorage.setItem("connected", JSON.stringify(connectedArr));
 
     clientsArr = JSON.parse(localStorage.getItem("activeSubscriptions"));
     clientsArr.push(data.activeSubscriptions);
